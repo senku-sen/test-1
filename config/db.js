b1c72f0d144d5f0710101851d48d70b545b3fd8f
Eajay Carl Abergos Bathan <ecbathan@gbox.adnu.edu.ph>
2025-10-05 14:20:40 +0700

    env/db

---
+++ config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(Process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (erorr){
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
